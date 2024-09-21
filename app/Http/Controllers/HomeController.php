0726ef3385d033e14f5d90222c8ef96fe9555d2c
Samsh001 <l.samshuijzen@uu.nl>
2024-09-21 20:09:17 +0200

    fix issues after laravel updates

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -40,7 +40,7 @@ class HomeController extends Controller
     {
         
         
-        return view('home');
+        return view('admin.home');
     }
     
     public function removeDataset()
