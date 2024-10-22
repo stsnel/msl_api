3fbcb4c7c7942380223ddb72e47ec68c8848f9e0
Samsh001 <l.samshuijzen@uu.nl>
2024-10-22 09:59:49 +0200

    add link from lab map popup to detail page

--- resources/views/frontend/labs-map.blade.php
+++ resources/views/frontend/labs-map.blade.php
@@ -71,7 +71,7 @@
                     <script>
         				function onEachFeature(feature, layer) {
                             if (feature.properties) {                                
-                                var popupContent = `<h5>${feature.properties.title}</h5><p>${feature.properties.msl_organization_name}</p><a href="">view lab information</a>`;
+                                var popupContent = `<h5>${feature.properties.title}</h5><p>${feature.properties.msl_organization_name}</p><a href="/lab/${feature.properties.name}">view lab information</a>`;
 
                                 layer.bindPopup(popupContent);
                             }
