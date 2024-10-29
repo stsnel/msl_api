869c4a595757c521c76b111a921a25bab83b8bcc
Samsh001 <l.samshuijzen@uu.nl>
2024-10-29 10:29:07 +0100

    add fields to lab detail view

--- resources/views/frontend/lab-detail.blade.php
+++ resources/views/frontend/lab-detail.blade.php
@@ -30,7 +30,62 @@
                                         {!! $data['msl_description_html'] !!}
                                     </p>
                                 </div >
-                                    
+                                
+                                @if (array_key_exists("msl_website", $data))
+                                <br>
+                                <div class="w-full pt-5 pb-5 flex flex-row">
+                                    <div class="w-1/3">
+                                        <h4 class="text-left">Website</h4>
+
+                                    </div>
+                                    <div class="w-full ">
+                                        <p>{{ $data['msl_website'] }}</p>
+                                    </div>
+                                </div>
+                                @endif
+
+                                @if (array_key_exists("msl_domain_name", $data))
+                                <br>
+                                <div class="w-full pt-5 pb-5 flex flex-row">
+                                    <div class="w-1/3">
+                                        <h4 class="text-left">Domain</h4>
+
+                                    </div>
+                                    <div class="w-full ">
+                                        <p>{{ $data['msl_domain_name'] }}</p>
+                                    </div>
+                                </div>
+                                @endif
+
+                                @if (array_key_exists("msl_organization_name", $data))
+                                <br>
+                                <div class="w-full pt-5 pb-5 flex flex-row">
+                                    <div class="w-1/3">
+                                        <h4 class="text-left">Organization name</h4>
+
+                                    </div>
+                                    <div class="w-full ">
+                                        <p>{{ $data['msl_organization_name'] }}</p>
+                                    </div>
+                                </div>
+                                @endif
+
+                                <br>
+                                <div class="w-full pt-5 pb-5 flex flex-row">
+                                    <div class="w-1/3">
+                                        <h4 class="text-left">Address</h4>                            
+                                    </div>
+                                    <div class="w-full ">
+                                        <p>
+                                            @if ($data['msl_address_street_1'] !== "") {{ $data['msl_address_street_1'] }}<br> @endif
+                                            @if ($data['msl_address_street_2'] !== "") {{ $data['msl_address_street_2'] }}<br> @endif
+                                            @if ($data['msl_address_postalcode'] !== "") {{ $data['msl_address_postalcode'] }}<br> @endif
+                                            @if ($data['msl_address_city'] !== "") {{ $data['msl_address_city'] }}<br> @endif
+                                            @if ($data['msl_address_country_name'] !== "") {{ $data['msl_address_country_name'] }}<br> @endif
+                                        </p>
+                                    </div>
+                                </div>
+                                
                                 
 
                             </div>
