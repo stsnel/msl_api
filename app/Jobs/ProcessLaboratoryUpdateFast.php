cf2b9386083416d7c100c2d054b62769f4d8d60e
Samsh001 <l.samshuijzen@uu.nl>
2024-12-17 12:16:53 +0000

    create seperate job for updating fast vocabulary

--- app/Jobs/ProcessLaboratoryUpdateFast.php
+++ app/Jobs/ProcessLaboratoryUpdateFast.php
@@ -7,7 +7,6 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use App\Models\Laboratory;
 use App\fast\Fast;
 use App\Models\Keyword;
 use App\Models\LaboratoryUpdateFast;
